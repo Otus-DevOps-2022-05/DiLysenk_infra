738073eb6ba6bdb889839220cc98b92b71a38b10
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2022-07-12 14:07:00 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 apt update
-apt -y install wireguard wireguard-tools
-ufw disable
 apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
