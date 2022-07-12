9027948b7cbcdfc9384df17b74b43e39059096b2
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2022-07-12 16:48:39 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y mongodb
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
+sudo systemctl enable mongod
+sudo systemctl start mongod
