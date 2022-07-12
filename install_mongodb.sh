89d3c4fa41e1a6d5097f2fba23fda25446e5da09
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2022-07-12 15:08:06 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-get -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -;
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list;
-sudo apt-get update;
-sudo apt-get install -y mongodb-org
-systemctl start mongod;
-systemctl enable mongod;
-systemctl status mongod;
+sudo apt-get update
+sudo apt-get install -y mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
