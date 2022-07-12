8294e6af523ca6d9daba3312afd53437330ff4f9
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2022-07-12 16:58:34 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y mongodb
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb http://mirrors.aliyun.com/mongodb/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install -y mongodb-org
 sudo systemctl enable mongod
 sudo systemctl start mongod
