9dd3ed55946f406bf5efef97eb5e0c790ef0ce1b
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2022-07-12 11:01:55 +0300

    fix mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-apt-get install -y mongodb-org
+echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
 systemctl status mongod
