1e7c1e9c71b1d8c5fdc550d6e40bc56a70f24c7b
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2022-07-27 12:50:04 +0300

    page 47

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-echo "sleep 3m for install updates"; sleep 5m; echo "start install ruby"
+echo "sleep 3m for install updates"; sleep 3m; echo "start install ruby"
 sudo apt install -y ruby-full ruby-bundler build-essential
 # check ruby
 ruby -v
